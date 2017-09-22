1a39734b5ed21236cb6674e1533c79dfacaaff63
sundayliu <martinkro@aliyun.com>
2017-09-22 17:10:41 +0800

    process

--- Beautiful/Process/main.cpp
+++ Beautiful/Process/main.cpp
@@ -1,10 +1,22 @@
 #include "process.h"
+#include "src/qapplist.h"
 #include <QtWidgets/QApplication>
-
+#include <QFile>
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	Process w;
-	w.show();
+
+	//Process w;
+	//w.show();
+
+	QFile file(":/uistyle");
+	file.open(QFile::ReadOnly);
+	qApp->setStyleSheet(file.readAll());
+	file.close();
+
+	QAppList win;
+	win.setWindowIcon(QIcon(":/logo"));
+	win.show();
+
 	return a.exec();
 }
